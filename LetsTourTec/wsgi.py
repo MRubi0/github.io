e6a1dbfbace29215ee0c742433c1eb99dbe074c7
MRubi0 <miguel_ortiz_rejon@hotmail.com>
2025-05-05 01:29:34 +0100

    rt7

--- LetsTourTec/wsgi.py
+++ LetsTourTec/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for LetsTourTec project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
 from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
@@ -14,4 +5,10 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LetsTourTec.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'LTtApp', 'static'))
+application = WhiteNoise(
+    application,
+    root=os.path.join(
+        os.path.dirname(os.path.dirname(__file__)),
+        'LTtApp', 'static'
+    )
+)
